9adc4efe73e9f373e7c77c2ec3d553d6a06c3b1a
titan-65 <teckcare2011@gmail.com>
2022-09-23 22:04:26 -0400

    removed button from index

--- components/MainNavbar.vue
+++ components/MainNavbar.vue
@@ -90,12 +90,12 @@ const links = [
       
       </div>
 
-      <a
+      <!-- <a
         class="block px-6 py-2.5 mt-4 font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto"
         href="#"
       >
         Get started
-      </a>
+      </a> -->
     </div>
   </nav>
 </template>
